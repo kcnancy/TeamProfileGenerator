c59495b77409fa6efcbdfb65a6cb768971c029d6
Nancy McBride <njmcbride224@gmail.com>
2021-03-02 20:57:10 -0600

    adding lib files updated index file

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const { stringify } = require("querystring");
+const generateManager = renderFile.createManager
+const generateEngineer = renderFile.createEngineer
+const generateIntern = renderFile.createIntern
+const renderTeam = renderFile.renderMembers
+
+inquirer.prompt()
 
 const team = [
-
-    {name: "mgrname",
+    {
+    name: "mgrname",
     type: "input",
     message: "Please enter the name of the manager.",
     },
 
-    {name: "empid",
+    {
+    name: "empid",
     type: "input",
     message: "Please enter employee id",
     },
-    {name: "email",
+
+    {
+    name: "email",
     type: "input",
     message: "Please enter email address",
+    },
+
+    {
+    name: "office",
+    type: "input",
+    message: "What is your office number?",
+    },
+
+    {
+    name:"emp-type",
+    type: "confirm",
+    message: "Would you like to add a team member?",
+    },
+
+.then((answer) => {
+
+}))
+    message,
+
+    //choices: ["engineer", "intern", "all finished, build my team"]
+    },
+    .then ((res)) => {
+        if (res.start ===)
     }
 
+
 ]
+])
+function writeToFile (data) {
+    const Myteam = buildMyteam (data)
+    fs.writeFileSync ("./dist/buildMyteam.js", Myteam, error =>{
+        if (error) console.log(error)
+    })
+}
+
+function init() {inquirer.prompt(team).then(data => {
+    writeToFile(data)
+})}
+
+init();
